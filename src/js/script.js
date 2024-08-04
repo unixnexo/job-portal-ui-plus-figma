1c871ee265789c24e8b2c9f7755b34758afa894f
unixnexo <unixnexo@gmail.com>
2024-08-04 15:33:03 +0330

    dev

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+/**
+ * toggle X svg to clear all checkboxes related to the form
+ */
+function toggleXsvgToClearAllCheckboxes(formId, svgId) {
+    const form = document.getElementById(formId);
+    const clearAll = document.getElementById(svgId);
+    const checkboxes = document.querySelectorAll(`#${formId} input[type="checkbox"]`);
+    form.addEventListener('input', () => {
+        let counter = 0;
+        checkboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+            counter += 1;
+        } 
+        });
+
+        // show/hide X svg
+        if (counter >= 1) {
+            clearAll.classList.remove('hidden');
+        } else {
+            clearAll.classList.add('hidden');
+        }
+    });
+}
+toggleXsvgToClearAllCheckboxes('jobtitle-filter-form', 'jobtitle-filter-form-clear-svg');
+toggleXsvgToClearAllCheckboxes('experience-filter-form', 'experience-filter-form-clear-svg');
+toggleXsvgToClearAllCheckboxes('salary-filter-form', 'salary-filter-form-clear-svg');
+toggleXsvgToClearAllCheckboxes('location-filter-form', 'location-filter-form-clear-svg');
+toggleXsvgToClearAllCheckboxes('gender-filter-form', 'gender-filter-form-clear-svg');
+toggleXsvgToClearAllCheckboxes('conditions-filter-form', 'conditions-filter-form-clear-svg');
